cb6d790e2cd3e0fe4d7da03b2d72740354c7bd11
Lacey Umamoto <lcumamoto@gmail.com>
2021-05-09 22:59:35 -0700

    finished footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,41 +1,24 @@
 import React from "react";
-import {
-  Container,
-  Grid,
-  Header,
-  List,
-  Segment,
-  Divider
-} from "semantic-ui-react";
+import { Container, Segment } from "semantic-ui-react";
 
 const Footer = () => {
   return (
-    <Container>
-    <footer>&copy; 2017 Company, Inc.</footer>
-    </Container>
+    <Segment>
+      <footer>
+        <Container textAlign="center">
+          <p>
+            <a href="https://github.com/RateMyHackathon">Rate My Hackathon</a>,
+            2021. Made with 💙 by&nbsp;
+            <a href="https://github.com/lumamoto" className="px-1 text-muted">
+              Lacey Umamoto
+            </a>
+            .
+          </p>
+        </Container>
+      </footer>
+    </Segment>
   );
 };
 
 export default Footer;
-
-// const Footer = () => {
-//   return (
-//     <div className="pt-5">
-//       <footer className="footer ml-auto py-3 bg-light">
-//         <div className="container">
-//           <span className="text-muted">
-//             Rate My Hackathon 2021. Made with 💙 by
-//             <a href="https://github.com/lumamoto" className="px-1 text-muted">
-//               Lacey Umamoto
-//             </a>
-//             {/* <a href="https://github.com/RateMyHackathon" className="px-1 text-muted">
-//               Source Code
-//             </a> */}
-//           </span>
-//         </div>
-//       </footer>
-//     </div>
-//   );
-// };
-
-// export default Footer;
+ 
